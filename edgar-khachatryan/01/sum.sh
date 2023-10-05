9cf57798f1a44bd15775305e827b393eea428d23
Edgar6455 <edgarkhachatryan645@gmail.com>
2023-10-05 19:32:49 +0400

    01/01 done

--- edgar-khachatryan/01/sum.sh
+++ edgar-khachatryan/01/sum.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+if [[ $# == 1 ]]
+then
+	echo "argument should be passed";
+	exit 1;
+fi
+
 limit=$1
 
 sum=0
 
 while read -r num
 do
-  if [ "$sum" -ge "$limit" ]
+  if [[ $sum -ge $limit ]]
   then
     echo $sum
     exit 1
