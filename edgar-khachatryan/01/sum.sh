09281c05f9b0252b940973d753fc28796308f2e8
Edgar6455 <edgarkhachatryan645@gmail.com>
2023-10-05 20:31:41 +0400

    01/02 done

--- edgar-khachatryan/01/sum.sh
+++ edgar-khachatryan/01/sum.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ $# == 1 ]]
+if [[ $# != 1 ]]
 then
-	echo "argument should be passed";
-	exit 1;
+    echo "argument should be passed";
+    exit 1;
 fi
 
 limit=$1
@@ -12,12 +12,12 @@ sum=0
 
 while read -r num
 do
-  if [[ $sum -ge $limit ]]
-  then
-    echo $sum
-    exit 1
-  fi
-  ((sum=sum+num))
+    if [[ $sum -ge $limit ]]
+    then
+        echo $sum
+        exit 1
+    fi
+    ((sum=sum+num))
 done
 
 echo $sum
