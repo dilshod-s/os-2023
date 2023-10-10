6feda8d168b2b3dbc1b8038703b4aafaa73e8c06
Gla1v3 <gla1v3@Linux.myguest.virtualbox.org>
2023-10-10 14:27:14 +0400

    Modified

--- samvel-arabyan/01/rename-images.sh
+++ samvel-arabyan/01/rename-images.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-for var in $(ls *.jpg)
-do
-tmp=$(stat $var -c "%y" | awk '{print $1}') 
-mv ./${var} ./${tmp}-${var}
-done
+for var in *.jpg 
+  do
+  if [[ "$var" =~ [0-9]{4}-[0-9]{2}-[0-9]{2}-.*\.jpg  ]] 
+  then
+ echo "Skipping!" 
+  else
+creation_date=$(stat $var -c "%y" | awk '{print $1}')
+new_name="${creation_date}-${var}"
+mv ./${var} ./${new_name}
+  fi
+ 
+  done
 
 
