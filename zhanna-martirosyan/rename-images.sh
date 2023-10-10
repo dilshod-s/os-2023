247d260db81ba52ccfbdb8ce84e8bae9e64b1ce9
ZhannaArm <146818984+ZhannaArm@users.noreply.github.com>
2023-10-10 13:08:15 +0400

    Update rename-images.sh

--- zhanna-martirosyan/rename-images.sh
+++ zhanna-martirosyan/rename-images.sh
@@ -2,13 +2,12 @@
 
 for image in *.jpg
 do
-    if [ -f "$image" ]; then
-        date=$(stat -c %y "$image" | cut -d ' ' -f 1)
-
-        new="${date}-${image}"
-        mv "$image" "$new"
-
-        echo "New name for $image is $new"
+    if [ -f "$image" ] 
+	then
+        	date=$(date -r "$image" "+%Y-%m-%d")
+        	new="$date-$image"
+        	mv "$image" "$new"
+        	echo "New name for $image is $new"
     fi
 done
 
