f98f273b702462fb50401ab71d36fec392aaad8e
ZhannaArm <146818984+ZhannaArm@users.noreply.github.com>
2023-10-17 16:34:15 +0400

    Update main.c

--- zhanna-martirosyan/02/main.c
+++ zhanna-martirosyan/02/main.c
@@ -6,38 +6,38 @@
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 3 && argc != 2) {
-        fprintf(stderr, "%s\n", argv[0]);
+    if(argc != 3 && argc != 2) {
+        fprintf(stderr, "Invalid input for %s\n", argv[0]);
         return 1;
     }
 
     if(argc == 2){
         const char *option = argv[1];
-        if (strcmp(option, "-l") == 0) {
+        if(strcmp(option, "-l") == 0) {
             int file_descriptor = 0;
 
             int lineCount = countLines(file_descriptor);
             printf("Number of lines: %d\n", lineCount);
         }
-        else if (strcmp(option, "-w") == 0) {
+        else if(strcmp(option, "-w") == 0) {
             int file_descriptor = 0;
 
             int wordCount = countWords(file_descriptor);
             printf("Number of words: %d\n", wordCount);
         }
         else{
-            fprintf(stderr, "%s\n", argv[0]);
+            fprintf(stderr, "Invalid input for %s\n", argv[0]);
             return 1;
         }
     }
-    else if (argc == 3) {
+    else if(argc == 3) {
         const char *option = argv[1];
         const char *name = argv[2];
 
-        if (strcmp(option, "-l") == 0) {
+        if(strcmp(option, "-l") == 0) {
             int file_descriptor = open(name, O_RDONLY);
 
-            if (file_descriptor == -1) {
+            if(file_descriptor == -1) {
                 perror("Error opening file");
                 return 1;
             }
@@ -46,10 +46,10 @@ int main(int argc, char *argv[]) {
             printf("Number of lines in %s: %d\n", name, lineCount);
             close(file_descriptor);
         }
-        else if (strcmp(option, "-w") == 0) {
+        else if(strcmp(option, "-w") == 0) {
             int file_descriptor = open(name, O_RDONLY);
 
-            if (file_descriptor == -1) {
+            if(file_descriptor == -1) {
                 perror("Error opening file");
                 return 1;
             }
@@ -59,7 +59,7 @@ int main(int argc, char *argv[]) {
             close(file_descriptor);
         }
         else{
-            fprintf(stderr, "%s\n", argv[0]);
+            fprintf(stderr, "Invalid input for %s\n", argv[0]);
             return 1;
         }
     }
