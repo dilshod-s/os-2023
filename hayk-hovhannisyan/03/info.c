b9b555ca52deee08d409d77131bb496c9ccf01f8
Hayk2508 <hovahnnisayn@mail.ru>
2023-10-20 19:03:38 +0400

    task 03 done

---
+++ hayk-hovhannisyan/03/info.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Error!\n");
+        return 1;
+    }
+
+    struct stat fileStat;
+
+    if (stat(argv[1], &fileStat) == -1) {
+        perror("stat");
+        return 2;
+    }
+
+    printf("Type: ");
+    if (S_ISREG(fileStat.st_mode)) {
+        printf("file\n");
+    } else if (S_ISDIR(fileStat.st_mode)) {
+        printf("directory\n");
+    } else if (S_ISLNK(fileStat.st_mode)) {
+        printf("symbolic link\n");
+    } else {
+        printf("other\n");
+    }
+
+    printf("Name: %s\n", argv[1]);
+    printf("Path: %s\n", realpath(argv[1], NULL));
+    printf("Size: %ld байт\n", fileStat.st_size);
+    printf("Block size: %ld байт\n", fileStat.st_blksize);
+    printf("Number of blocks: %ld\n", fileStat.st_blocks);
+    printf("Permissions %o\n", fileStat.st_mode & 0777);
+
+    struct tm createdTime;
+    struct tm modifiedTime;
+
+    localtime_r(&fileStat.st_ctime, &createdTime);
+    localtime_r(&fileStat.st_mtime, &modifiedTime);
+
+    char createdTimeString[20];
+    char modifiedTimeString[20];
+
+    strftime(createdTimeString, sizeof(createdTimeString), "%Y-%m-%d %H:%M:%S", &createdTime);
+    strftime(modifiedTimeString, sizeof(modifiedTimeString), "%Y-%m-%d %H:%M:%S", &modifiedTime);
+
+    printf("Date of creation.: %s\n", createdTimeString);
+    printf("Last modified date: %s\n", modifiedTimeString);
+    printf("Number of inode: %lu\n", fileStat.st_ino);
+    printf("Number of hard links.: %lu\n", fileStat.st_nlink);
+
+    return 0;
+}
