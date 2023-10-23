53e9ba49c81570cfab5ebab949853507a180514c
Davio-2002 <khach.dav2002@gmail.com>
2023-10-24 01:18:52 +0400

    fixed date duplication error

--- davit-khachatryan/01/rename-images.sh
+++ davit-khachatryan/01/rename-images.sh
@@ -2,12 +2,17 @@
 
 for jpg_file in *.jpg; do
   if [ -f "$jpg_file" ]; then
-    creation_date=$(stat -c %y "$jpg_file" | cut -d ' ' -f 1)
-    new_name="${creation_date}-${jpg_file}"
-    if [ ! -e "$new_name" ]; then
-      mv "$jpg_file" "$new_name"
-      echo "File renamed: $jpg_file -> $new_name"
+    # Extract the date from the filename if it exists
+    creation_date=$(echo "$jpg_file" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}')
+
+    if [ -z "$creation_date" ]; then
+      creation_date=$(stat -c %y "$jpg_file" | cut -d ' ' -f 1)
+      new_name="${creation_date}-${jpg_file}"
+      
+      if [ ! -e "$new_name" ]; then
+        mv "$jpg_file" "$new_name"
+        echo "File renamed: $jpg_file -> $new_name"
+      fi
     fi
   fi
 done
-
