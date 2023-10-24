1c4ea94cddb28c9f5e8cc16141dab5019c302329
Ashot <ashot.khachatryan204@gmail.com>
2023-10-24 17:34:09 +0400

    03 update

--- Ashot-Khachatryan/03/main.cpp
+++ Ashot-Khachatryan/03/main.cpp
@@ -25,12 +25,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    char* full_path = realpath(filename, NULL);
+    std::cout << "Full Path: " << (full_path ? full_path : "Error retrieving path") << std::endl;
+    free(full_path);
+    
     std::cout << "Type: " << (S_ISREG(file_info.st_mode) ? "File" : (S_ISDIR(file_info.st_mode) ? "Directory" : "Unknown")) << std::endl;
     std::cout << "Name: " << filename << std::endl;
 
     std::cout << "Size: " << file_info.st_size << " bytes" << std::endl;
     std::cout << "Block Size: " << file_info.st_blksize << " bytes" << std::endl;
-    std::cout << "Number of Blocks: " << file_info.st_blocks << std::endl;
+    std::cout << "Number of Blocks: " << file_info.st_blocks / 8 << std::endl;
+
 
     std::cout << "Permissions: " << std::oct << (file_info.st_mode & 0777) << std::dec << std::endl;
 
