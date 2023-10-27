75363edeecaf609425f6e85a0501718fa852f724
ProizvolniyA <146282731+ProizvolniyA@users.noreply.github.com>
2023-10-28 01:20:22 +0400

    wordcount.c
    infinity cycle was improved

--- armen-mkrtchyan/02/wordcount.c
+++ armen-mkrtchyan/02/wordcount.c
@@ -1,59 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> //simvolicheskie constanti
-
-unsigned count_lines(int fd){
-    
-    unsigned lines = 0;
-    char buffer;
-    int read_status;
-
-    for(int read_status = read(fd, &buffer, 1); read_status > 0;){
-        
-        if (buffer == '\n'){
-            
-            ++lines;
-        }
-    }
-
-    if(read_status < 0){
-        
-        perror("read");
-        exit(EXIT_FAILURE);
-    }
-
-    return lines;
-}
+#include <stdbool.h> //dlya bulleanov
 
 unsigned count_words(int fd){
     
     unsigned words = 0;
-    char buffer[4096];
-
+    bool inword = false;
+    char buffer[1024];
     int read_status;
-    int word_started = 0;
 
-    for(int read_status = read(fd, buffer, sizeof(buffer)); read_status > 0;){
+    while(read_status = read(fd, &buffer, 1) > 0){
+        
+        for(int i = 0; i < read_status; ++i){
+             
+            if(buffer[i] == ' '|| buffer[i] == '\n' || buffer[i] == '\t'){
+                
+                inword = false;                
+            }
+            else if(!inword){
+                
+                inword = true;
+                ++words;
+            }     
+        } 
+        
+        
+    }
+    return words;
+}
+
+unsigned count_lines(int fd){
+    
+    unsigned lines = 1;
+    char buffer[1024];
+    int read_status; 
+    
+    while(read_status = read(fd, buffer, 1) > 0){
         
         for(int i = 0; i < read_status; ++i){
             
-            if(buffer[i] == ' ' || buffer[i] == '\n' || buffer[i] == '\t'){
+            if(buffer[i] == '\n'){
                 
-                word_started = 0;
-                
-            }else if(!word_started){
-                
-                ++words;
-                word_started = 1;
-            }
-        }
+                ++lines;
+            }  
+        }      
     }
-
-    if(read_status < 0){
-        
-        perror("read");
-        exit(EXIT_FAILURE);
-    }
-
-    return words;
+    
+    return lines;
 }
