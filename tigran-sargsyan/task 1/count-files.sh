9ecd2ff94371277158bde8cb4128203b80ee3039
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2023-10-27 21:55:23 +0400

    Update count-files.sh

--- tigran-sargsyan/task 1/count-files.sh
+++ tigran-sargsyan/task 1/count-files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 count_files() {
-    local dir=$1
-    local count=$(ls -1 "$dir" | wc -l)
-    echo "$count"
+    local dir=$1 
+    local count=$(ls -1 "$dir" | wc -l) local //подсчет строк в dir
+    echo "$count" //like return
 }
 
-count_files "$1"
+count_files "$1" //call function
