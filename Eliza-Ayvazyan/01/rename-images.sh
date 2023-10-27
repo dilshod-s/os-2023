ab2c0449e5339ce338d8512e39f6b2f97e889185
ElizaAyv <146765471+ElizaAyv@users.noreply.github.com>
2023-10-27 22:09:26 +0400

    task 01 update

--- Eliza-Ayvazyan/01/rename-images.sh
+++ Eliza-Ayvazyan/01/rename-images.sh
@@ -4,9 +4,12 @@ function rename(){
   for file in *.jpg; do
     if [[ -f "$file" ]]; 
     then
-      date=$(stat -c %y "$file" | awk -F ' ' '{print $1}')
-      new_name="${date}-${file}"
-      mv "$file" "$new_name"
+      if [[ -z "$(echo "$file" | grep -o '^[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}')" ]];
+      then
+        date=$(stat -c %y "$file" | awk -F ' ' '{print $1}')
+        new_name="${date}-${file}"
+        mv "$file" "$new_name"
+      fi
     fi
   done
 }
