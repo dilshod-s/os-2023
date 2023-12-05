6bc007796ecf65aef9695156de17abaac321b71c
ZhannaArm <146818984+ZhannaArm@users.noreply.github.com>
2023-12-05 12:53:50 +0400

    Update thread.с

--- zhanna-martirosyan/06/thread.c
+++ zhanna-martirosyan/06/thread.c
@@ -7,16 +7,16 @@
 #define N 100000001
 
 int* arr; //массив случайных чисел
-int numThreads; //количество потоков
+int countThreads; //количество потоков
 uint64_t* localResult; //массив локальных результатов
 
 //функция, которую выполняет каждый поток
 void* worker(void* data) {
     int threadIndex = *((int*)data); //индекс потока
-    int blockSize = N / numThreads; //размер блока для каждого потока вычисляет
+    int blockSize = N / countThreads; //размер блока для каждого потока вычисляет
     int start = threadIndex * blockSize; //начало блока
     int end; //конец блока
-    if (threadIndex == numThreads - 1) {
+    if (threadIndex == countThreads - 1) {
         end = N;
     } else {
         end = (threadIndex + 1) * blockSize;
@@ -34,14 +34,14 @@ void* worker(void* data) {
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        printf("Использование: %s <количество_потоков>\n", argv[0]);
+        printf("Аргумента должно быть 2: название файла и количество потоков.\n");
         return 1;
     }
 
-    numThreads = atoi(argv[1]); //инициализируем количество потоков (atoi - из строки в целое число)
+    countThreads = atoi(argv[1]); //инициализируем количество потоков (atoi - из строки в целое число)
 
-    if (numThreads <= 0) {
-        printf("Количество потоков должно быть положительным числом\n");
+    if (countThreads <= 0) {
+        printf("Количество потоков должно быть положительным числом.\n");
         return 1;
     }
 
@@ -50,31 +50,31 @@ int main(int argc, char* argv[]) {
         arr[i] = rand();
     }
 
-    localResult = (uint64_t*)malloc(numThreads * sizeof(uint64_t)); //массив для локальных результатов размером в количество потоков
+    localResult = (uint64_t*)malloc(countThreads * sizeof(uint64_t)); //массив для локальных результатов размером в количество потоков
 
     //запускаем потоки
-    pthread_t threads[numThreads]; //тут находятся ID потоков
-    int threadInd[numThreads]; //тут индексы потоков(от 0 до (numThreads - 1))
+    pthread_t threads[countThreads]; //тут находятся ID потоков
+    int threadInd[countThreads]; //тут индексы потоков(от 0 до (numThreads - 1))
 
     struct timeval start, end; //создаем структуру, в которой будет храниться время. start и end для того,
     // чтобы показать начало и конец выполнения программы
     gettimeofday(&start, NULL); //записываем текущее время в start. Это нужно для замера времени выполнения программы.
     // NULL для использования системного времени
 
-    for (int i = 0; i < numThreads; ++i) { //создаем и запускаем потоки, передавая каждому индекс потока
+    for (int i = 0; i < countThreads; ++i) { //создаем и запускаем потоки, передавая каждому индекс потока
         //индексы нужны для определения, какой блок данных будет обработан каждым потоком
         threadInd[i] = i;
         pthread_create(&threads[i], NULL, worker, &threadInd[i]); //создаем поток
     }
 
-    for (int i = 0; i < numThreads; ++i) { //завершаем потоки
+    for (int i = 0; i < countThreads; ++i) { //завершаем потоки
         pthread_join(threads[i], NULL);
     }
 
     gettimeofday(&end, NULL); //текущее время сохраняем в end
 
     uint64_t result = 0;
-    for (int i = 0; i < numThreads; ++i) { //вычисляем xor из локальных результатов(общую побитовую сумму)
+    for (int i = 0; i < countThreads; ++i) { //вычисляем xor из локальных результатов(общую побитовую сумму)
         result ^= localResult[i];
     }
 
