685e8ced2a348f7dc6a81e4c32933dcf34b27c1e
GiDrA766 <124083184+GiDrA766@users.noreply.github.com>
2023-12-05 14:12:18 +0400

    Update threads.cpp

--- Eremia_Tamamyan/task6/threads.cpp
+++ Eremia_Tamamyan/task6/threads.cpp
@@ -4,42 +4,59 @@
 #include <pthread.h>
 #include <random>
 #include <vector>
-unsigned int xor_result = 0;
-std::default_random_engine generator;
-std::uniform_int_distribution<unsigned int> distribution(0, 2000);
 
+
+
+unsigned int xor_result = 0;
+std::mt19937 generator;  // Измененный генератор
+std::uniform_int_distribution<unsigned int> distribution(0, 2000);
+std::vector<unsigned int> local_xors;
 void *worker(void *data);
 
 int main() {
-  unsigned int N;
-  std::cout << "Enter the number of elements \n";
-  std::cin >> N;
+  unsigned int N = 100000000;
+
   int ThreadsAmount;
   std::cout << "Enter the number of threads: \n ";
   std::cin >> ThreadsAmount;
+  local_xors.resize(ThreadsAmount);
   unsigned int for_each_thread = N / ThreadsAmount;
   std::vector<pthread_t> threads(ThreadsAmount);
   auto start = std::chrono::high_resolution_clock::now();
-  unsigned int *args = new unsigned int{for_each_thread};
-  for (int i = 0; i < ThreadsAmount; i++) {
-    pthread_create(threadп[i], NULL, worker, args);
+
+  for (unsigned int i = 0; i < ThreadsAmount; i++) {
+    unsigned int *args = new unsigned int[2]{i, for_each_thread};
+    pthread_create(&threads[i], NULL, worker, args);
   }
-  for (int i = 0; i < ThreadsAmount; i++) 
+  
+  for (int i = 0; i < ThreadsAmount; i++) {
     pthread_join(threads[i], NULL);
-    delete args;
+  }
   auto end = std::chrono::high_resolution_clock::now();
-  auto duration =
-      std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-  std::cout << "Time taken: " << duration.count() << " microseconds"
-            << std::endl;
+  
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+  std::cout << "Time taken: " << duration.count() << " microseconds" << std::endl;
+  for(auto i: local_xors)
+  {
+    xor_result ^= i;
+  }
   std::cout << "XOR result: " << xor_result << std::endl;
+
   return 0;
 }
+
 void *worker(void *data) {
   unsigned int *args = static_cast<unsigned int *>(data);
-  unsigned int range = *args;
+  unsigned int range = args[1];
+  unsigned int index = args[0];
+  unsigned int local_xor_result = 0;
+
   for (int i = 0; i < range; i++) {
-    xor_result ^= distribution(generator);
+    local_xor_result ^= distribution(generator);
   }
+  local_xors[index]=local_xor_result;
+  
+  delete[] args;
   pthread_exit(NULL);
 }
