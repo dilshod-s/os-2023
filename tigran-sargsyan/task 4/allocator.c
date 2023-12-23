411067a4f3b09edf2957b409f2764db06f42b9af
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2023-12-23 10:04:47 +0400

    Update allocator.c

--- tigran-sargsyan/task 4/allocator.c
+++ tigran-sargsyan/task 4/allocator.c
@@ -1,73 +1,96 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MEMSIZE 2 << 16
-#define PGSIZE 2 << 8
+#define MEMSIZE (2 << 16)
+#define PGSIZE (2 << 8)
 
 struct Memory {
-	char buffer[MEMSIZE];
+    char buffer[MEMSIZE];
 };
 
-void initMem(struct Memory *mem) {
-	memset(&mem->buffer, 0, MEMSIZE);
-	// Выделяем первый блок для индекса блока
-	mem->buffer[0] = 1;
+void initMem(struct Memory* mem) {
+    memset(&mem->buffer, 0, MEMSIZE);
+    mem->buffer[0] = 1;
 }
 
 int index2address(int idx) {
-	return idx * PGSIZE;
+    return idx * PGSIZE;
 }
 
 int address2index(int addr) {
-	return addr / PGSIZE;
+    return addr / PGSIZE;
 }
 
-// Выделяем максимум 1 страницу памяти
-void *alloc_1(struct Memory *mem, size_t sz) {
-	if (sz > PGSIZE) {
-		return NULL;
-	}
+void* alloc(struct Memory* mem, size_t sz) {
+    if (sz > PGSIZE) {
+        // Search for free space
+        for (int i = 0; i < MEMSIZE; i += PGSIZE) {
+            if (mem->buffer[i] == 0) {
+                break;
+            }
+        }
 
-	// Поиск свободного места
-	for (int i = 1; i < PGSIZE; i++) {
-		if (mem->buffer[i] == 0) {
-			break;
-		}
-	}
+        // Check if space is available
+        if (i + PGSIZE > MEMSIZE) {
+            return NULL;
+        }
 
-	// Отмечаем пространство как выделенное
-	mem->buffer[i] = 1;
+        // Mark space as allocated
+        mem->buffer[i] = 1;
 
-	// Возвращаем адрес
-	int addr = index2address(i);
-	return &mem->buffer[addr];
-};
+        // Return address
+        return &mem->buffer[i];
+    } else {
+        // Search for free space
+        int i = 1;
+        for (; i < PGSIZE; i++) {
+            if (mem->buffer[i] == 0) {
+                break;
+            }
+        }
 
-int free_1(struct Memory *mem, void *ptr) {
-	if ((size_t) ptr % PGSIZE != 0) {
-		return -1;
-	}
+        // Mark space as allocated
+        mem->buffer[i] = 1;
 
-	int index = address2index(
-		(int) (ptr - (void *) mem->buffer)
-	);
+        // Return address
+        int addr = index2address(i);
+        return &mem->buffer[addr];
+    }
+}
 
-	mem->buffer[index] = 0;
-	return 0;
+int free(struct Memory* mem, void* ptr) {
+    if ((size_t)ptr % PGSIZE != 0) {
+        return -1;
+    }
+
+    int index = address2index((int)(ptr - (void*)mem->buffer));
+
+    mem->buffer[index] = 0;
+    return PGSIZE;
+}
+
+int clean(struct Memory* mem) {
+    memset(&mem->buffer, 0, MEMSIZE);
+    mem->buffer[0] = 1;
+    return MEMSIZE;
 }
 
 int main() {
+    struct Memory mem;
+    initMem(&mem);
 
-	struct Memory mem;
-	memset(&mem.buffer, 0, MEMSIZE);
+    void* ptr1 = alloc_1(&mem, 128);
+    printf("Payload (alloc_1) // pointer = %lx\n", (size_t)ptr1);
 
-	void *ptr = &mem.buffer;
-	printf("Payload // pointer = %lx\n",(size_t)mem.buffer);
+    void* ptr2 = alloc(&mem, 256);
+    printf("Payload (alloc) // pointer = %lx\n", (size_t)ptr2);
 
-	ptr = alloc_1(&mem, 100);
-	printf("Payload // pointer = %lx\n",(size_t)ptr);
+    free(&mem, ptr1);
 
-	free_1(&mem, ptr);
+    void* ptr3 = alloc(&mem, 64);
+    printf("Payload (alloc) // pointer = %lx\n", (size_t)ptr3);
 
-	return 0;
+    clean(&mem);
+
+    return 0;
 }
