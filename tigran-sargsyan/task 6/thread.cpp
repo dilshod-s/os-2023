a070f1aa5eba223e9dedfb73fc8b59588fbc558c
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2023-12-23 13:46:33 +0400

    Update thread.cpp

--- tigran-sargsyan/task 6/thread.cpp
+++ tigran-sargsyan/task 6/thread.cpp
@@ -1,58 +1,57 @@
 #include <iostream>
 #include <thread>
-#include <random>
 #include <vector>
 #include <chrono>
+#include <random>
 
-std::vector<int> random_num(int n){
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<> dis(1, 100);
-  std::vector<int> v(n);
-  for(int i = 0; i < n; i++) {
-    v[i] = dis(gen);
-  }
-  return v;
+void calculate_xor_sum(std::vector<int>& nums, int start, int end, int& result) {
+    for (int i = start; i < end; ++i) {
+        result ^= nums[i];
+    }
 }
 
-int xor_sum(std::vector<int> v, int start, int end, int& result){
-  int sum = 0;
-  for(int i = start; i < end; i++) {
-    sum ^= v[i];
-  }
-  return sum;
-}
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <number of threads>" << std::endl;
+        return 1;
+    }
 
-int main(int argc,char **argv) {
-  if(argc != 2){
-    std::cerr << "Error";
-  }
-  int K = std::stoi(argv[1]);
-  int N = 1000000000;
-  int thread_num = N/K;
-  std::vector<std::thread> threads(K);
-  std::vector<int> v = random_num(N);
-  std::vector<int> sums(K);
-  
-  auto start_time = std::chrono::high_resolution_clock::now();
-  for (int i = 0; i < K; i++) {
-      threads[i] = std::thread(xor_sum, std::ref(v), i * thread_num, (i + 1) * thread_num, std::ref(sums[i]));
-  }
+    int num_threads = std::stoi(argv[1]);
+    int N = 100000000;
+    int chunk_size = N / num_threads;
 
-  for (int i = 0; i < K; i++) {
-      threads[i].join();
-  }
+    std::vector<int> nums(N);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 255);
 
-  int final_result = 0;
+    for (int i = 0; i < N; ++i) {
+        nums[i] = dis(gen);
+    }
 
-  for (int i = 0; i < K; i++) {
-      final_result ^= sums[i];
-  }
-  
-  auto end_time = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    std::vector<std::thread> threads(num_threads);
+    std::vector<int> results(num_threads);
+    auto start_time = std::chrono::high_resolution_clock::now();
 
-  std::cout << "XOR sum: " << final_result << std::endl;
-  std::cout << "Time taken: " << duration.count() << " ms" << std::endl;
-  return 0;
+    for (int i = 0; i < num_threads; ++i) {
+        threads[i] = std::thread(calculate_xor_sum, std::ref(nums), i * chunk_size, (i + 1) * chunk_size, std::ref(results[i]));
+    }
+
+    for (int i = 0; i < num_threads; ++i) {
+        threads[i].join();
+    }
+
+    int final_result = 0;
+
+    for (int i = 0; i < num_threads; ++i) {
+        final_result ^= results[i];
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+
+    std::cout << "XOR sum: " << final_result << std::endl;
+    std::cout << "Time taken: " << duration.count() << " ms" << std::endl;
+
+    return 0;
 }
