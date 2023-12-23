d1db47354b45ef17de4d44fd1d9b1fe3eccabba8
Tigo16 <146854464+Tigo16@users.noreply.github.com>
2023-12-23 14:30:56 +0400

    Update myterm.cpp

--- tigran-sargsyan/task 5/myterm.cpp
+++ tigran-sargsyan/task 5/myterm.cpp
@@ -3,13 +3,29 @@
 #include <vector>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sstream>
+#include <stdio.h>
+#include <string.h>
 
 void exCom(const std::string& com) {
+    std::istringstream ist(com);
+    std::vector<std::string> args;
+    std::string arg;
+    while (ist >> arg)
+    {
+        args.push_back(arg);
+    }
+    char *command_and_arguments[args.size() + 1];
+    for (size_t i = 0; i < args.size(); i++)
+    {
+        command_and_arguments[i] = strdup(args[i].c_str());
+    }
+    command_and_arguments[args.size()] = nullptr;
     pid_t pid = fork();
     if (pid == 0) {
         // Дочерний процесс - выполняет команду
-        execlp(com.c_str(), com.c_str(), nullptr);
-        // Если execlp вернется, значит произошла ошибка
+        execvp(command_and_arguments[0], command_and_arguments);
+        // Если execvp вернется, значит произошла ошибка
         std::cerr << "Ошибка выполнения команды\n";
         exit(EXIT_FAILURE);
     } else if (pid > 0) {
